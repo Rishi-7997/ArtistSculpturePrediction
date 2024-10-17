251978eba537ab93fdf240cfa8c01c8ded6fbf3a
Rishi-7997 <rishisharma.308@gmail.com>
2024-10-17 23:41:54 +0530

    Added frontend using react

--- sculpture/api/urls.py
+++ sculpture/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 urlpatterns = [
      # path to predict urls
     path('api/predict', views.predict, name  = 'predict'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
